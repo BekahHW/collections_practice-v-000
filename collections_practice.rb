bce9378ad7d31022c1844e5e154177a300b42fff
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-12 18:00:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(words)
+  words.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  new_array = array[0], array[2], array[1] 
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each 
+end
